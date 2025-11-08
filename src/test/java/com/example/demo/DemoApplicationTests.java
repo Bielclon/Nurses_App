b385781548f15ad365ad301f075d06a15a0687f5
Takkun <rodrimd231@gmail.com>
2025-11-08 20:28:59 +0100

    alternative fix

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,7 +3,10 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.example.Nurse.DemoApplication;
+
+@SpringBootTest(classes = DemoApplication.class)
+
 class DemoApplicationTests {
 
 	@Test
